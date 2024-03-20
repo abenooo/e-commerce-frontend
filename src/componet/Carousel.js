aa787341ef779f0f044780ad229c1b863619230a
Abenezer <abenezerkifle000@gmail.com>
2024-03-20 08:33:40 -0700

    Finsh carousal

--- src/componet/Carousel.js
+++ src/componet/Carousel.js
@@ -1,9 +1,62 @@
-import React from 'react'
+"use client";
+import React from "react";
+import Slider from "react-slick";
+import Slide from "./Slide";
 
+// carousal setting
 const Carousel = () => {
-  return (
-    <div>Carousel</div>
-  )
-}
+  let settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    pauseOnHover: true,
+  };
+  // data
+  const slideData = [
+    {
+      id: 0,
+      img: "/banner-1.jpg",
+      title: "Trending Item",
+      mainTitle: "WOMEN'S LATEST FASHION SALE",
+      price: "$20",
+    },
+    {
+      id: 1,
+      img: "/banner-2.jpg",
+      title: "Trending Accessories",
+      mainTitle: "MODERN SUNGLASSES",
+      price: "$15",
+    },
+    {
+      id: 2,
+      img: "/banner-3.jpg",
+      title: "Sale Offer",
+      mainTitle: "NEW FASHION SUMMER SALE",
+      price: "$30",
+    },
+  ];
 
-export default Carousel
+  return (
+    <div>
+      <div className="container pt-6 lg:pt-0">
+        <Slider {...settings}>
+          {slideData.map((item) => {
+            return (
+              <Slide
+                key={item.id}
+                img={item.img}
+                title={item.title}
+                mainTitle={item.title}
+                price={item.price}
+              />
+            );
+          })}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
