63fb5fd21b3e29c8950f8240948f868feb470afd
Abenezer <abenezerkifle000@gmail.com>
2024-03-27 13:10:42 -0700

    Add ionicons for drop down menu

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,45 @@
-"use client"
+// "use client";
+// import Carousel from "@/componet/Carousel";
+// import Product from "@/componet/Product";
+// import Testimonial from "@/componet/Testimonial";
+// import Banner from "@/componet/Banner";
+// import AOS from "aos";
+// import "aos/dist/aos.css";
+// import React from "react";
+
+// export default function Home() {
+//   const BannerData = {
+//     discount: "30% OFF",
+//     title: "Fine Smile",
+//     date: "10 Jan to 28 Jan",
+//     image: "/asset/headphone.png",
+//     title2: "Air Solo Bass",
+//     title3: "Winter Sale",
+//     title4:
+//       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+//   };
+//   React.useEffect(() => {
+//     AOS.init({
+//       duration: 800,
+//       easing: "ease-in-sine",
+//       delay: 100,
+//       offset: 100,
+//     });
+//     AOS.refresh();
+//   }, []);
+//   return (
+    
+//     <main>
+//       {/* <Carousel /> */}
+
+//       <Product />
+//       {/* <Testimonial/> */}
+//       <Banner data={BannerData} />
+//     </main>
+//   );
+// }
+"use client";
+import Script from 'next/script'; // Correctly import Script from 'next/script'
 import Carousel from "@/componet/Carousel";
 import Product from "@/componet/Product";
 import Testimonial from "@/componet/Testimonial";
@@ -6,6 +47,7 @@ import Banner from "@/componet/Banner";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import React from "react";
+
 export default function Home() {
   const BannerData = {
     discount: "30% OFF",
@@ -14,9 +56,9 @@ export default function Home() {
     image: "/asset/headphone.png",
     title2: "Air Solo Bass",
     title3: "Winter Sale",
-    title4:
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+    title4: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
   };
+
   React.useEffect(() => {
     AOS.init({
       duration: 800,
@@ -26,12 +68,25 @@ export default function Home() {
     });
     AOS.refresh();
   }, []);
+
   return (
-   <main>
-    <Carousel />
-    <Product/>
-    {/* <Testimonial/> */}
-    <Banner data={BannerData}/>
-   </main>
+    <>
+      <Script
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+        strategy="beforeInteractive"
+        type="module"
+      />
+      <Script
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
+        strategy="beforeInteractive"
+        nomodule
+      />
+      <main>
+        <Carousel />
+        <Product />
+        {/* <Testimonial/> */}
+        <Banner data={BannerData} />
+      </main>
+    </>
   );
 }
