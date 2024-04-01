5b32dc649e71aaccf1e46fa70fa2d2cbd4998584
macboock <macboock@macboocks-MacBook-Pro.local>
2024-04-01 06:40:46 -0700

    Use react icons instead of ionnicons

--- src/componet/Navbar/NavLinks.js
+++ src/componet/Navbar/NavLinks.js
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import { Link } from "next/link";
 import { links } from "./Mylinks";
 import Image from "next/image";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { IoChevronDown, IoChevronUp } from "react-icons/io5";
+
 const NavLinks = () => {
   const [heading, setHeading] = useState("");
   const [subHeading, setSubHeading] = useState("");
   return (
     <>
       {links.map((link) => (
-        <div>
+        <div key={link.name}>
           <div className="px-3  text-left md:cursor-pointer group">
             <h1
               className="py-7 flex justify-between items-center md:pr-0 pr-5 group"
@@ -19,19 +22,15 @@ const NavLinks = () => {
             >
               {link.name}
               <span className="text-xl md:hidden inline">
-                <ion-icon
-                  name={`${
-                    heading === link.name ? "chevron-up" : "chevron-down"
-                  }`}
-                ></ion-icon>
+                {heading === link.name ? <IoChevronUp /> : <IoChevronDown />}
               </span>
               <span className="text-xl md:mt-1 md:ml-2  md:block hidden group-hover:rotate-180 group-hover:-mt-2">
-                <ion-icon name="chevron-down"></ion-icon>
+                <IoChevronDown />
               </span>
             </h1>
             {link.submenu && (
               <div>
-                <div className="absolute w-full left-0 top-20 hidden group-hover:md:block hover:md:block border-4 border-gray-800">
+                <div className="absolute w-full left-0 top-20 hidden group-hover:md:block hover:md:block border-4 border-gray-100 ">
                   <div className="py-3">
                     <div
                       className="w-4 h-4 left-3 absolute 
@@ -39,10 +38,6 @@ const NavLinks = () => {
                     ></div>
                   </div>
                   <div className="bg-white p-5 grid grid-cols-4 gap-5">
-                    {/* Image container now spans 2 out of 4 columns */}
-                  
-
-                    {/* Items container (remaining content) */}
                     <div className="col-span-2 grid grid-cols-2 gap-5">
                       {link.sublinks.map((mysublinks, index) => (
                         <div key={index} className="col-span-1">
@@ -50,11 +45,9 @@ const NavLinks = () => {
                             {mysublinks.Head}
                           </h1>
                           <ul>
-                            {" "}
-                            {/* Each group of sublinks should be in a separate list */}
                             {mysublinks.sublink.map((slink, subIndex) => (
                               <li
-                                key={subIndex}
+                                key={subIndex} // Added unique key
                                 className="text-sm text-gray-600 my-2.5 hover:text-primary"
                               >
                                 <a href={slink.link}>{slink.name}</a>
@@ -84,36 +77,38 @@ const NavLinks = () => {
           >
             {/* sublinks */}
             {link.sublinks.map((slinks) => (
-              <div>
+              <div key={slinks.Head}>
                 <div>
                   <h1
-                    onClick={() =>
-                      subHeading !== slinks.Head
-                        ? setSubHeading(slinks.Head)
-                        : setSubHeading("")
-                    }
-                    className="py-4 pl-7 font-semibold md:pr-0 pr-5 flex justify-between items-center "
+                    className="py-7 flex justify-between items-center md:pr-0 pr-5 group"
+                    onClick={() => {
+                      subHeading === slinks.Head
+                        ? setSubHeading("")
+                        : setSubHeading(slinks.Head);
+                    }}
                   >
                     {slinks.Head}
-
-                    <span className="text-xl md:mt-1 md:ml-2 inline">
-                      <ion-icon
-                        name={`${
-                          subHeading === slinks.Head
-                            ? "chevron-up"
-                            : "chevron-down"
-                        }`}
-                      ></ion-icon>
+                    <span className="text-xl md:hidden inline">
+                      {subHeading === slinks.Head ? (
+                        <FaChevronUp />
+                      ) : (
+                        <FaChevronDown />
+                      )}
+                    </span>
+                    <span className="text-xl md:mt-1 md:ml-2  md:block hidden group-hover:rotate-180 group-hover:-mt-2">
+                      <FaChevronDown />
                     </span>
                   </h1>
+
                   <div
                     className={`${
                       subHeading === slinks.Head ? "md:hidden" : "hidden"
                     }`}
                   >
-                    {slinks.sublink.map((slink) => (
-                      <li className="py-3 pl-14">
-                        <li href={slink.link}>{slink.name}</li>
+                    {slinks.sublink.map((slink, index) => (
+                      <li key={index} className="py-3 pl-14">
+                        <a href={slink.link}>{slink.name}</a>{" "}
+                        {/* Use <a> instead of <li> */}
                       </li>
                     ))}
                   </div>
