8a0bb51981527de8005367cf0d47d922e830901a
macboock <macboock@macboocks-MacBook-Pro.local>
2024-04-02 04:49:31 -0700

    Add mobile version

--- src/componet/Navbar/Navbar.js
+++ src/componet/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 // First, import useState and Link
-"use client"
+"use client";
 import React, { useState } from "react";
 import Link from "next/link"; // Make sure this is the correct import
 // Import specific icons from React Icons
@@ -13,9 +13,8 @@ const Navbar = () => {
     <nav className="bg-white sticky top-0 z-50">
       <div className="flex items-start font-medium justify-around">
         <div className="z-50 p-5 md:w-auto w-full flex justify-between">
-          {/* <img src="../image-4.jpg" alt="logo" className="md:cursor-pointer h-10 w-10" /> */}
           <div className="text-3xl md:hidden" onClick={() => setOpen(!open)}>
-            <ion-icon name={`${open ? "close" : "menu"}`}></ion-icon>
+            {open ? <IoClose /> : <IoMenu />}
           </div>
         </div>
         <ul className="md:flex  relative hidden uppercase items-center gap-8 font-[Poppins]">
@@ -27,10 +26,26 @@ const Navbar = () => {
         </ul>
         {/* Mobile Menu */}
         <ul
-          className={`absolute md:hidden bg-white w-full px-4 py-24 top-0 left-0 transform ${open ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out`}
+          className={`absolute md:hidden bg-white w-full px-4 py-24 top-0 left-0 transform ${
+            open ? "translate-x-0" : "-translate-x-full"
+          } transition-transform duration-300 ease-in-out`}
         >
           <li>
             <Link href="/">Home</Link>
+          </li>
+          <NavLinks />
+        </ul>
+        {/* Mobile nav */}
+        <ul
+          className={`
+        md:hidden bg-white fixed w-full top-0 overflow-y-auto bottom-0 py-24 pl-4
+        duration-500 ${open ? "left-0" : "left-[-100%]"}
+        `}
+        >
+          <li>
+            <Link href="/" className="py-7 px-3 inline-block">
+              Home
+            </Link>
           </li>
           <NavLinks />
         </ul>
